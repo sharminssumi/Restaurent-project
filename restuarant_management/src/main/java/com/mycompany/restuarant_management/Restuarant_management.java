dc4713290f5dd79c4dc2e09a31df15778157a67d
Sharmin  Sumi <140547252+sumiosd@users.noreply.github.com>
2023-08-11 21:11:26 +0600

    Add files via upload

---
+++ restuarant_management/src/main/java/com/mycompany/restuarant_management/Restuarant_management.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.restuarant_management;
+
+/**
+ *
+ * @author User
+ */
+public class Restuarant_management {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ restuarant_management/src/main/java/restaurent/LoginAndSignup.java
@@ -0,0 +1,11 @@
+
+package restaurent;
+
+public class LoginAndSignup {
+    public static void main(String[] args) {
+        Login LoginFrame  = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+    }
+}
